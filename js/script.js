1c1c74893a30a7b7d4fafa1153c074de4e7cae5a
mellek89 <melaniekoester89@gmail.com>
2026-03-13 09:00:39 -0300

    menucontainer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const menuContainer = document.getElementById("menuContainer");
 const video = document.getElementById("intro");
+const isIndex = window.location.pathname === "/" || window.location.pathname.endsWith("index.html");
 
 
+if (isIndex) {
 video.addEventListener("ended", () => {
   video.style.display = "none"; // Video ausblenden
   menuContainer.style.display = "flex"; // Menü anzeigen
 });
+}
+
+
+if (!isIndex) {
+  menuContainer.style.display = "flex";
+}
