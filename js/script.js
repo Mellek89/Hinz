81310ffeade7c0271b3fe45a5bd5a25314d89eaf
mellek89 <melaniekoester89@gmail.com>
2026-03-14 16:57:21 -0300

    herbsideen

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@ const menuContainer = document.getElementById("menuContainer");
 const video = document.getElementById("intro");
 const isIndex = window.location.pathname === "/" || window.location.pathname.endsWith("index.html");
 const welcome = document.getElementById("welcome");
-const welcomeText = document.getElementById("welcomeText"); 
+const welcomeText = document.getElementById("welcomeText");
+
 const nav = document.getElementById("nav");
 if (isIndex) {
 video.addEventListener("ended", () => {
   video.style.display = "none"; 
   welcome.style.display = "none";
-  welcomeText.style.display = "flex";
   menuContainer.style.display = "flex"; 
+  welcomeText.style.display = "flex"; 
   nav.style.display = "flex"; // Menü anzeigen
 
 });
