799567c789723ec5c4238a5141e8429487176385
mellek89 <melaniekoester89@gmail.com>
2026-03-14 08:17:46 -0300

    hinzrecov

--- js/script.js
+++ js/script.js
@@ -3,17 +3,21 @@ const video = document.getElementById("intro");
 const isIndex = window.location.pathname === "/" || window.location.pathname.endsWith("index.html");
 const welcome = document.getElementById("welcome");
 const welcomeText = document.getElementById("welcomeText"); 
+const nav = document.getElementById("nav");
 if (isIndex) {
 video.addEventListener("ended", () => {
   video.style.display = "none"; 
   welcome.style.display = "none";
   welcomeText.style.display = "flex";
-  menuContainer.style.display = "flex"; // Menü anzeigen
+  menuContainer.style.display = "flex"; 
+  nav.style.display = "flex"; // Menü anzeigen
+
 });
 }
 
 
 if (!isIndex) {
   menuContainer.style.display = "flex";
+    nav.style.display = "flex"; // Menü anzeigen
 }
 
